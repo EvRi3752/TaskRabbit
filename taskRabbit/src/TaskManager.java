db5788ec08ca747304a349e6020d954d668afd00
Ervin <aboutervinbandong@gmail.com>
2026-01-15 16:37:07 +1100

    made progress on taskManager

--- taskRabbit/src/TaskManager.java
+++ taskRabbit/src/TaskManager.java
@@ -1,9 +1,45 @@
 import java.util.ArrayList;
-import Habbit.java;
-import Task.java;
+import java.util.List;
+import java.util.stream.Collectors; 
 
 public class TaskManager {
-    ArrayList<Task>; 
+    
+    private final List<Task> tasks; 
+
+    public TaskManager(){
+        this.tasks = new ArrayList<>(); 
+    }
+
+    // core method
+
+    public void addTask(Task task) {
+        if(task == null) {
+            throw new IllegalArgumentException("Task cannot be null"); 
+        }
+
+        tasks.add(task); 
+    }
+
+    public boolean removeTask(int id){
+        return tasks.removeIf(t -> t.getId() == id);
+    }
+
+    public List<Task> getAllTasks() {
+        return new ArrayList<>(tasks);
+    }
+
+    // Search method
+
+    public List<Task> searchByTitle(String keyword){
+        if (keyword == null || keyword.isBlank()){
+            return List.of(); 
+        }
+
+        String lower = keyword.toLowerCase(); 
+        return tasks.stream()
+                 .filter(t -> t.getTitle().toLowerCase().contains(lower))
+                 .collect(Collectors.toList()); 
+    }
 
     
 }
